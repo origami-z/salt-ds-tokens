1c382f0f09a32eb719d2dd4d723ecec2620f15f0
Shirley Li <shirleyli@shirleys-mbp.corp.adobe.com>
2024-06-03 11:16:51 -0700

    test: created more tests to test 2+ tokens and entire existing schema

--- tools/diff-generator/test/renameChecker.test.js
+++ tools/diff-generator/test/renameChecker.test.js
@@ -12,8 +12,12 @@ governing permissions and limitations under the License.
 
 import test from "ava";
 import tokenDiff from "../src/index.js";
-import original from "./test-schemas/basicOriginalToken.json" with { type: "json" };
-import updated from "./test-schemas/basicRenamedToken.json" with { type: "json" };
+import original from "./test-schemas/basic-original-token.json" with { type: "json" };
+import updated from "./test-schemas/basic-renamed-token.json" with { type: "json" };
+import originalTwoOrMore from "./test-schemas/several-original-tokens.json" with { type: "json" };
+import updatedTwoOrMore from "./test-schemas/several-renamed-tokens.json" with { type: "json" };
+import originalEntireSchema from "./test-schemas/entire-schema.json" with { type: "json" };
+import updatedEntireSchema from "./test-schemas/entire-schema-renamed.json" with { type: "json" };
 
 const expected = [
   {
@@ -22,6 +26,52 @@ const expected = [
   },
 ];
 
+const expected1 = [
+  {
+    oldname: "swatch-border-color",
+    newname: "swatch-color",
+  },
+  {
+    oldname: "swatch-border-opacity",
+    newname: "swatch-opacity",
+  },
+];
+
+const expected2 = [
+  {
+    oldname: "swatch-border-opacity",
+    newname: "swatch-opacity",
+  },
+  {
+    oldname: "swatch-disabled-icon-border-color",
+    newname: "swatch-disabled-border-color",
+  },
+  {
+    oldname: "table-row-hover-color",
+    newname: "table-col-hover-color",
+  },
+  {
+    oldname: "table-row-hover-opacity",
+    newname: "table-col-hover-opacity",
+  },
+  {
+    oldname: "table-selected-row-background-opacity-non-emphasized",
+    newname: "table-selected-row-background-opacity-definitely-emphasized",
+  },
+  {
+    oldname: "table-selected-row-background-opacity-non-emphasized-hover",
+    newname: "table-selected-row-background-opacity-ultra-emphasized-hover",
+  },
+];
+
 test("basic test to see if diff catches rename", (t) => {
   t.deepEqual(tokenDiff(original, updated), expected);
+});
+
+test("several tokens in each schema test to see if diff catches rename", (t) => {
+  t.deepEqual(tokenDiff(originalTwoOrMore, updatedTwoOrMore), expected1);
+});
+
+test("existing schema test to see if diff catches rename", (t) => {
+  t.deepEqual(tokenDiff(originalEntireSchema, updatedEntireSchema), expected2);
 });
